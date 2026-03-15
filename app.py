b2b548cec059e2523fbc3355392110164d8a77df
dandubhavya2 <dandubhavya2@gmail.com>
2026-03-15 13:41:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask,render_template,request
+import cv2
+import numpy as np
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/detect', methods=['POST'])
+def detect():
+
+    file = request.files['image']
+    path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+    file.save(path)
+
+    img = cv2.imread(path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+
+    _, thresh = cv2.threshold(blur,120,255,cv2.THRESH_BINARY_INV)
+
+    disease_pixels = np.sum(thresh==255)
+    total_pixels = thresh.size
+
+    percent = (disease_pixels/total_pixels)*100
+
+    if percent > 5:
+        result = "Leaf is Diseased"
+    else:
+        result = "Leaf is Healthy"
+
+    return render_template("result.html", result=result)
+
+
+if __name__== '__main__':
+    app.run(debug=True)
